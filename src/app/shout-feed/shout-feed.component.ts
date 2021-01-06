7af690712116b32f07a227cdd9ffef8bb75b74c3
Amey Khaire <72918936+ameykhaire0@users.noreply.github.com>
2021-01-06 14:02:26 +0530

    added shout feed

--- src/app/shout-feed/shout-feed.component.ts
+++ src/app/shout-feed/shout-feed.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shout-feed',
   templateUrl: './shout-feed.component.html',
-  styleUrls: ['./shout-feed.component.scss']
+  styleUrls: ['./shout-feed.component.scss'],
 })
 export class ShoutFeedComponent implements OnInit {
+  faLike = faThumbsUp;
+  data = [
+    {
+      type: 'text',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla dolorum earum eaque delectus maxime quae ex eius eum deserunt alias, rerum quidem eos aliquam voluptatum, quibusdam facere esse commodi.',
+      createdAt: '2021-1-6',
+    },
+    {
+      type: 'audio',
+      content: 'This is audio',
+      media: '../../assets/audio.mp3',
+      createdAt: '2021-1-6',
+    },
+    {
+      type: 'video',
+      content: 'This is video',
+      media: '../../assets/video.mp4',
+      createdAt: '2021-1-6',
+    },
+    {
+      type: 'image',
+      content: 'This is image',
+      media: '../../assets/image.jpg',
+      createdAt: '2021-1-6',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
