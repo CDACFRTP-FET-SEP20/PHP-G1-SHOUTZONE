ee091a5e6987545df8f0a125151a5d836c4f4b9b
Yash <yashch@cybage.com>
2021-01-08 15:19:04 +0530

    auth service updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,75 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
- import { User } from '../model/user';
+import { User } from '../model/user';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
-
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   user = new User();
   loginForm: FormGroup;
 
+  submitted = false;
 
-  submitted =false;
+  constructor(private service: AuthService, private router: Router) {}
 
-  constructor(private service: AuthService, private router: Router) { }
-
-
-
-  ngOnInit() :void {
-
+  ngOnInit(): void {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required,Validators.minLength(7)]),
-    })
-
-
-}
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
 
   userLogin() {
-
-
     this.service.loginUserFromRemote(this.user).subscribe(
       (data) => {
-
         //console.log(data);
-          data.status_code!= Number
+        data.status_code != Number;
 
         switch (data.status_code) {
           case 500:
-            alert("please fill valid credentials");
+            alert('please fill valid credentials');
             break;
           case 400:
-            alert("You are Successfully Registerd...You Will be Verified within 24 Hours!!");
+            alert(
+              'You are Successfully Registerd...You Will be Verified within 24 Hours!!'
+            );
             break;
           case 300:
-            alert("please fill valid credentials for Users");
+            alert('please fill valid credentials for Users');
             break;
           default:
-            sessionStorage.setItem('username', data.user.username);
-            sessionStorage.setItem('user', data.user);
-           // sessionStorage.setItem('token', data.token);
-           // sessionStorage.setItem('userid', data.user.id);
-          this.router.navigateByUrl('/home');
+            this.service.storeUserData(data.user);
+            this.router.navigateByUrl('/home');
             break;
         }
-       },
-      (err) => { alert(err) },
-      () => { }
-    )
-
+      },
+      (err) => {
+        alert(err);
+      },
+      () => {}
+    );
   }
-
-
-
 }
-
-
-
