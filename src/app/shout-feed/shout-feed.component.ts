1986c7cd830a95c1fd06ee8ffe9cee57813c1f21
Anvesha905 <72918976+Anvesha905@users.noreply.github.com>
2021-01-08 16:40:42 +0530

    done shout fetch

--- src/app/shout-feed/shout-feed.component.ts
+++ src/app/shout-feed/shout-feed.component.ts
@@ -12,7 +12,44 @@ import{ShoutsService} from '../services/shouts.service';
   styleUrls: ['./shout-feed.component.scss'],
 })
 export class ShoutFeedComponent implements OnInit {
- 
+
+  user_id:any;
+  value:any;
+
+session:any;
+  mediaPath:any='http://127.0.0.1:8000';
+
+userShout:any;
+
+ data: Observable<any>;
+  constructor(private post: ShoutsService,private activer:ActivatedRoute,private r:Router) {
+   this.data = this.post.getData();
+  // this.session=sessionStorage.getItem('user_id');
+  // this.userShout=this.post.getShoutsById(this.session);
+
+  }
+
+
+  ngOnInit(): void {
+
+
+    this.session=sessionStorage.getItem('user_id');
+    console.log(this.session);
+    this.userShout=this.post.getShoutsById(this.session);
+    // console.log(this.userShout);
+
+
+  }
+  deleteOwnShout(id:number)
+  {
+
+      this.post.deleteOwnShout(id).subscribe(data=>{
+      console.log("data"+data);
+      window.alert('Deleted Successfully');
+       this.ngOnInit();
+    });
+  }
 
 }
+
 
