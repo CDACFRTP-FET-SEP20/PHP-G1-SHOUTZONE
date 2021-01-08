5e241be00c9baea70df098e4cbb5e4cc11233577
Amey Khaire <72918936+ameykhaire0@users.noreply.github.com>
2021-01-08 18:56:52 +0530

    ui updates

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+// import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<CommentsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shout/shout.component.ts
+++ src/app/shout/shout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { CommentsComponent } from '../comments/comments.component';
 
 @Component({
   selector: 'app-shout',
@@ -10,4 +11,9 @@ export class ShoutComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CommentsComponent, {
+      data: { postId: 2 },
+    });
+  }
 }
