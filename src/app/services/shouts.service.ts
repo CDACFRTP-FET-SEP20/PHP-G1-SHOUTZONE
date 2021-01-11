a4f43af3b4564c4b046be869dbbbe66db60f2237
Anvesha905 <72918976+Anvesha905@users.noreply.github.com>
2021-01-11 15:04:22 +0530

    shout service updated

--- src/app/services/shouts.service.ts
+++ src/app/services/shouts.service.ts
@@ -32,11 +32,11 @@ export class ShoutsService {
      if(shout.shoutText != null)
      return this.httpClient.post('http://127.0.0.1:8000/api/shouts/add', formData);
   }
-//  getShoutsById(user_id:number):Observable<any>
-//  {
-//   let url = 'http://127.0.0.1:8000/api/show/'+user_id;
-//   return this.httpClient.get(url);
-// }
+ getShoutsById(user_id:number):Observable<any>
+ {
+  let url = 'http://127.0.0.1:8000/api/show/'+user_id;
+  return this.httpClient.get(url);
+}
 deleteOwnShout(id:number):Observable<any>{
   return this.httpClient.get('http://127.0.0.1:8000/api/destroy/'+id);
 }
