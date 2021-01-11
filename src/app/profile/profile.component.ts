59739267d3197f5bc5881654b6dac4eca8fe9b4b
Yash <yashch@cybage.com>
2021-01-11 18:46:31 +0530

    profile updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {Shout} from '../create-shout/Shout';
+import { Shout } from '../create-shout/Shout';
 import { Observable } from 'rxjs';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import{ShoutsService} from '../services/shouts.service';
+import { ShoutsService } from '../services/shouts.service';
+import { AuthService } from '../services/auth.service';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-profile',
@@ -11,47 +13,42 @@ import{ShoutsService} from '../services/shouts.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  user = {
-    username: 'amey khaire',
-    friends: 89,
-    shouts: 78,
-  };
-  user_id:any;
-  value:any;
+  user: User;
+  user_id: any;
+  value: any;
 
-session:any;
-  mediaPath:any='http://127.0.0.1:8000';
+  session: any;
+  mediaPath: any = 'http://127.0.0.1:8000';
 
-userShout:any;
+  userShout: any;
 
-
-  constructor(private post: ShoutsService,private activer:ActivatedRoute,private r:Router) {
-
-
-  }
-
+  constructor(
+    private post: ShoutsService,
+    private auth: AuthService,
+    private activer: ActivatedRoute,
+    private r: Router
+  ) {}
 
   ngOnInit(): void {
-
-    sessionStorage.setItem('user_id','2');
-    this.session=sessionStorage.getItem('user_id');
-    console.log(this.session);
-   this.userShout=this.post.getShoutsById(this.session);
-    console.log(this.userShout);
-
-
+    //sessionStorage.setItem('user_id', '12');
+    //this.session = sessionStorage.getItem('user_id');
+    this.user = this.auth.getUserDetails();
+    this.userShout = this.post.getShoutsById(this.user.id);
+    this.post.getShoutsById(this.user.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.userShout = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  deleteOwnShout(id:number)
-  {
-
-      this.post.deleteOwnShout(id).subscribe(data=>{
-      console.log("data"+data);
+  deleteOwnShout(id: number) {
+    this.post.deleteOwnShout(id).subscribe((data) => {
+      console.log('data' + data);
       window.alert('Deleted Successfully');
-       this.ngOnInit();
+      this.ngOnInit();
     });
   }
-
 }
-
-
-
