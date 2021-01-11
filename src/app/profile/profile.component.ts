9e6eae339c2af656d1ae0364842a4b78315f7f9a
Amey Khaire <72918936+ameykhaire0@users.noreply.github.com>
2021-01-11 16:13:40 +0530

    comment and create shout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,48 +14,56 @@ export class ProfileComponent implements OnInit {
 
   data = [
     {
+      id: 1,
       type: 'text',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla dolorum earum eaque delectus maxime quae ex eius eum deserunt alias, rerum quidem eos aliquam voluptatum, quibusdam facere esse commodi.',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'text',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla dolorum earum eaque delectus maxime quae ex eius eum deserunt alias, rerum quidem eos aliquam voluptatum, quibusdam facere esse commodi.',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'audio',
       content: 'This is audio',
       media: '../../assets/audio.mp3',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'video',
       content: 'This is video',
       media: '../../assets/video.mp4',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'image',
       content: 'This is image',
       media: '../../assets/image.jpg',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'audio',
       content: 'This is audio',
       media: '../../assets/audio.mp3',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'video',
       content: 'This is video',
       media: '../../assets/video.mp4',
       createdAt: '2021-1-6',
     },
     {
+      id: 1,
       type: 'image',
       content: 'This is image',
       media: '../../assets/image.jpg',
