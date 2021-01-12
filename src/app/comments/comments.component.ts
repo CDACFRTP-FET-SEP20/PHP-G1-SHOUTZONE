fbe8ce2344aa8f4066f811d26d693f9a53c5082b
Yash <yashch@cybage.com>
2021-01-12 13:51:08 +0530

    comments done

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 // import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from '../model/user';
+import { AuthService } from '../services/auth.service';
+import { ShoutsService } from '../services/shouts.service';
 
 @Component({
   selector: 'app-comments',
@@ -8,12 +11,52 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  comments: any;
+  user: User;
+  inputComment: string;
+  myDate: any;
   constructor(
     public dialogRef: MatDialogRef<CommentsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private shout: ShoutsService,
+    private auth: AuthService
   ) {}
 
+  listComments() {
+    this.shout.getComments(this.data.postId).subscribe((data) => {
+      console.log(data);
+
+      this.comments = data;
+    });
+  }
+
+  // deleteComment(){
+  //   this.user=this.auth.getUserDetails();
+  //   if(this.data.postId)
+  // }
+
+  postComment() {
+    if (this.inputComment != null) {
+      console.log('In Store', this.data.postId);
+
+      this.user = this.auth.getUserDetails(); //current logged in user
+      let data = {
+        shout_id: this.data.postId,
+        id: this.user.id,
+        comment: this.inputComment,
+      };
+      this.shout.storeComment(data).subscribe((data) => {
+        this.inputComment = '';
+        console.log('post done');
+        this.listComments();
+      });
+    }
+  }
+
   ngOnInit(): void {
+    console.log(this.data);
+
     console.log(this.data.postId);
+    this.listComments();
   }
 }
--- src/app/friends-list/friends-list.component.ts
+++ src/app/friends-list/friends-list.component.ts
@@ -15,6 +15,7 @@ export class FriendsListComponent implements OnInit {
   user_id: number;
   data: any;
   user: User;
+  searchedKeyword: string;
   constructor(
     private friends: FriendsService,
     private auth: AuthService,
