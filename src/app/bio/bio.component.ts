380bf83be5100328e9d182e30e67db275f88b2cc
Amey Khaire <72918936+ameykhaire0@users.noreply.github.com>
2021-01-16 16:34:46 +0530

    User Bio component added

---
+++ src/app/bio/bio.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: ['./bio.component.scss'],
+})
+export class BioComponent implements OnInit {
+  @Input() user: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.user);
+  }
+}
