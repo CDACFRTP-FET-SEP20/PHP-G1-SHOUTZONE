a2e0307ecfe24c60b5482cc4dcb8ba1cab54b425
Amey Khaire <72918936+ameykhaire0@users.noreply.github.com>
2021-01-17 14:49:48 +0530

    search friend functionality added added

---
+++ src/app/search-friend/search-friend.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FriendsService } from '../services/friends.service';
+import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-search-friend',
+  templateUrl: './search-friend.component.html',
+  styleUrls: ['./search-friend.component.scss'],
+})
+export class SearchFriendComponent implements OnInit {
+  user: any;
+  people: any;
+  data: {
+    user_id: any; //current logged in user
+    reciever: any;
+  };
+  keyword: string;
+
+  toShow: any;
+  constructor(
+    private friends: FriendsService,
+    private auth: AuthService,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  addFriend(id) {
+    console.log('inside Add', id);
+    this.user = this.auth.getUserDetails(); //current logged in user
+
+    let index = this.people.findIndex((ele) => ele.id === id);
+    this.people[index].found = false;
+
+    this.data = {
+      user_id: this.user.id, //current logged in user
+      reciever: id,
+    };
+    this.friends.sendFriendRequest(this.data).subscribe(
+      () => {
+        console.log('sent Request');
+        this._snackBar.open(
+          `Friend Request has been sent to ${this.people[index].username} `,
+          'Dismiss',
+          {
+            duration: 2000,
+          }
+        );
+
+        this.userList();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  userList() {
+    this.user = this.auth.getUserDetails(); //current logged in user
+
+    console.log(this.user.id);
+
+    //this.people = this.friends.getSuggestionList(this.user_id);
+    this.friends.getSuggestionList(this.user.id).subscribe((data) => {
+      this.people = data;
+      this.toShow = data.length;
+
+      Object.keys(this.people).map(
+        (ele) => (this.people[ele] = { ...this.people[ele], found: true })
+      );
+      console.log(this.people);
+    });
+  }
+
+  ngOnInit(): void {
+    this.userList();
+  }
+}
